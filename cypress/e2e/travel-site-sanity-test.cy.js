502a540172f97e1d487bce1ae8213fccdacf7192
Natalya Lechshenko <natalis125@gmail.com>
2022-11-02 02:03:37 -0500

    M4 - Cypress assignment

--- cypress/e2e/travel-site-sanity-test.cy.js
+++ cypress/e2e/travel-site-sanity-test.cy.js
@@ -20,13 +20,42 @@ describe('The sanity test suite for travel site', () => {
       cy.get('[data-cy="Flight"]').should('have.text',"flight")
    })
 
-     it('verify elements of Hotels tab', () => {
+     it('verify address box', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('#tab1 > form > :nth-child(1)').should("be.visible")
+    cy.contains('Going to').should('be.visible')
+    cy.get('[name="address"]').should('be.visible')
+    })
+
+    it('verify Check-in box', () => {
       cy.visit('http://localhost:3000')
-      cy.get('[data-cy="flight_chk"]').should("be.visible")
-      cy.get('[data-cy="cars_chk"]').should('be.visible')
-      //cy.get('#tab1 > form > .btn-submit').should('be.visible')
-      //cy.contains('search').should('be.visible')
-      cy.get('[type=submit]').should('be.visible')
-   })
+      cy.get('#tab1 > form > .row > :nth-child(1) > .input-group').should("be.visible")
+      cy.get('[id="input-start"]').should('be.visible')
+      })
+
+    it('verify Check-out box', () => {
+        cy.visit('http://localhost:3000')
+        cy.get('#tab1 > form > .row > :nth-child(2) > .input-group').should("be.visible")
+        cy.get('[id="input-end"]').should('be.visible')
+        })
+      
+        it('verify Travelers list', () => {
+        cy.visit('http://localhost:3000')
+        cy.get('#tab1 > form > .row > :nth-child(2) > .input-group').should("be.visible")
+        cy.get('#info').should("be.visible")
+        //cy.get('[class="dropdown-select show"]').should("not.be.visible")
+        cy.get(':nth-child(1) > .name').should("not.be.visible")
+        cy.get(':nth-child(2) > .name').should("not.be.visible")
+        })
+
+        it('verify elements of Hotels tab', () => {
+          cy.visit('http://localhost:3000')
+          cy.get('[data-cy="flight_chk"]').should("be.visible")
+          cy.get('[data-cy="cars_chk"]').should('be.visible')
+          //cy.get('#tab1 > form > .btn-submit').should('be.visible')
+          
+          cy.get('[type=submit]').should('be.visible')
+       })
+  
 
     })   
